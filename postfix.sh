479abcfb8f20e3166309ee7f905eb0ea040d73c2
Lewis <43108320+notthelewis@users.noreply.github.com>
2020-03-12 15:49:25 +0000

    Update postfix.sh

--- postfix.sh
+++ postfix.sh
@@ -1,1 +1,3 @@
-postqueue -p | tail -n +2 | awk 'BEGIN { RS = "" } /ianio@ianio\.com/ { print $1 }' | tr -d '*!'| postsuper -d -
+mailq # Gets the mail queue
+postqueue -p | tail -n +2 | awk 'BEGIN { RS = "" } /ianio@ianio\.com/ { print $1 }' | tr -d '*!'| postsuper -d - 
+# ^ Deletes everything from specific mailbox.
